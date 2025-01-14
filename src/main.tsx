bf3756f89863a9164f5a33e5bc92ae1ccbf468b9
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2025-01-14 22:38:26 +0100

    Cleanup and reproduce

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import * as stylex from "@stylexjs/stylex";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+export const example = stylex.defineVars({
+  etst: stylex.types.angle("20deg"),
+});
